1d9d6adae4b33e06c104e836fb6ef29fb86c5aac
Georgey <georgey.gerasimov@gmail.com>
2024-01-17 02:11:19 +0300

    add categories

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Goods } from '../../components/Goods/Goods';
 
 import style from './Main.module.scss';
+import { useEffect } from 'react';
+import { fetchCategories } from '../../store/categories/categories.slice';
+import { Catalog } from '../../components/Catalog/Catalog';
 
-export const Main = () => (
-  <main className={style.main}>
-    {/* <Catalog /> */}
-    <Goods />
-  </main>
-);
+export const Main = () => {
+  const dispatch = useDispatch();
+  const {
+    data: dataCategories,
+    loading: loadingCategories,
+    error: errorCategories,
+  } = useSelector((state) => state.categories);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  if (loadingCategories) return <div>Loading categories</div>;
+  if (errorCategories) return <div>Ошибка: {errorCategories}</div>;
+
+  return (
+    <main className={style.main}>
+      <Catalog data={dataCategories} />
+      <Goods />
+    </main>
+  );
+};
